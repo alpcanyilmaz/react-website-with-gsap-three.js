01e34449a22196a1689fe048f9b647b4094b35be
alpcanyilmaz <alpcanyilmazz@gmail.com>
2024-07-22 15:56:48 +0300

    added models, features and footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -8,6 +11,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
+      <Features />
+      <Footer />
     </main>
   );
 };
